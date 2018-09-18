a46e99e2a4663601296b1b70a384d4c6ff69f4ce
Radoslav Gyudyulev <radoslav.gyudyulev@gmail.com>
2018-09-18 15:34:08 +0300

    links

--- src/store.js
+++ src/store.js
@@ -1,16 +1,33 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  }
-})
+   state: {
+      title: 'My Custom Title',
+      links: [
+        'youtube.com',
+        'google.com',
+        'yahoo.com'
+      ]
+   },
+   getters: {
+     countLinks: state => {
+       return state.links.length
+     }
+   },
+   mutations: {
+    ADD_LINK: (state, link) => {
+      state.links.push(link)
+    },
+    REMOVE_LINK: (state, link) => {
+      state.links.splice(link, 1)
+    }
+   },
+   actions: {
+    removeLink: (context, link) => {
+      context.commit("REMOVE_LINK", link)
+    }
+   }
+});
